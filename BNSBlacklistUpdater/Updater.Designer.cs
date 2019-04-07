55b5d69c5b82fcaeed549d4877de4da882144077
kroulis <qinkroulis@gmail.com>
2019-04-07 02:02:41 -0700

    Adding initial code for updater and recordcreater

---
+++ BNSBlacklistUpdater/Updater.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace BNSBlacklistUpdater
+{
+    partial class Updater
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Updater
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(740, 658);
+            this.Name = "Updater";
+            this.Text = "BNSBlacklist Updater";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
